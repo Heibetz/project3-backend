1c7f7b8444971d8c26655b715eb4a350e4b8f793
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-11-03 10:38:14 -0600

    add exercise table to db

--- app/models/index.js
+++ app/models/index.js
@@ -8,6 +8,7 @@ import User from "./user.model.js";
 import Session from "./session.model.js";
 import Tutorial from "./tutorial.model.js";
 import Lesson from "./lesson.model.js"; 
+import Exercise from "./exercise.model.js";
 
 
 const db = {};
@@ -18,6 +19,7 @@ db.user = User;
 db.session = Session;
 db.tutorial = Tutorial;
 db.lesson = Lesson;
+db.exercise = Exercise;
 
 // foreign key for session
 db.user.hasMany(
@@ -53,6 +55,18 @@ db.lesson.belongsTo(
   db.tutorial,
   { as: "tutorial" },
   { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+
+// foreign key for exercises (created_by -> user)
+db.user.hasMany(
+  db.exercise,
+  { as: "exercises" },
+  { foreignKey: { name: "created_by", allowNull: false }, onDelete: "CASCADE" }
+);
+db.exercise.belongsTo(
+  db.user,
+  { as: "creator" },
+  { foreignKey: { name: "created_by", allowNull: false }, onDelete: "CASCADE" }
 );
 
 export default db;
